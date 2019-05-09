ff7dca70a01bab3a8333d4c6a07559aac6366998
PaulChen <bernie40916@gmail.com>
2019-05-09 20:30:50 +0800

    change fps to 60

--- Node-sockets/client/draw.js
+++ Node-sockets/client/draw.js
@@ -10,7 +10,8 @@ function setup() {
     for (var i = 0; i < MAX_STARS; i++) {
         stars[i] = new Star();
     }
-    rippleSetup()
+    // rippleSetup()
+    frameRate(60)
 
 }
 
@@ -128,50 +129,4 @@ class Star {
         stroke(255)
         line(px, py, sx, sy)
     }
-}
-
-
-function rippleSetup() {
-    pixelDensity(1)
-    cols = width
-    rows = height
-    for (let i = 0; i < cols; i++) {
-      current[i] = []
-      previous[i] = []
-      for (let j = 0; j < rows; j++) {
-        current[i][j] = 0
-        previous[i][j] = 0
-      }
-    }
-    previous[100][100] = 0
-}
-
-function drawRipple() {
-    loadPixels()
-    for (let i = 1; i < cols - 1; i++) {
-      for (let j = 1; j < rows - 1; j++) {
-        current[i][j] =
-          (previous[i - 1][j] + previous[i + 1][j] +
-            previous[i][j - 1] + previous[i][j + 1] +
-            previous[i - 1][j - 1] + previous[i - 1][j + 1] +
-            previous[i + 1][j - 1] + previous[i + 1][j + 1]
-          ) / 4 - current[i][j];
-        current[i][j] = current[i][j] * damping
-        let index = (i + j * cols) * 4;
-        pixels[index + 0] = current[i][j] * 255
-        pixels[index + 1] = current[i][j] * 255
-        pixels[index + 2] = current[i][j] * 255
-        pixels[index + 3] = 255
-      }
-    }
-    updatePixels()
-    
-    //swap buffers
-    let temp = previous
-    previous = current
-    current = temp
-}
-
-function mouseDragged() {
-    current[mouseX][mouseY] = 255
 }
